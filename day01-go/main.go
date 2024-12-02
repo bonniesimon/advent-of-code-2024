04a4822d29311600ea003c20ec2fa6193f9d7cca
Bonnie Simon <bonnie.developer@gmail.com>
2024-12-02 14:58:58 +0530

    parse inputs into data structure

--- day01-go/main.go
+++ day01-go/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,5 +19,24 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 	}
 
-	fmt.Println(string(data))
+	rows := strings.Split(string(data), "\n")
+
+	// Ignore empty array at the end due to EOF
+	rows = rows[:len(rows)-1]
+
+	var left []int
+	var right []int
+
+	for _, row := range rows {
+		fmt.Println(strings.Split(row, "   "))
+		col := strings.Split(row, "   ")
+		leftNumber, _ := strconv.Atoi(col[0])
+		rightNumber, _ := strconv.Atoi(col[1])
+
+		left = append(left, leftNumber)
+		right = append(right, rightNumber)
+	}
+
+	fmt.Println(left)
+	fmt.Println(right)
 }
